6c4a92b2d31b87e79b66511766abcb6a01149562
Nocs13 <nocs13@gmail.com>
2018-09-23 17:45:29 +0400

    ...

---
+++ kGame.cpp
@@ -0,0 +1,6 @@
+#include "kGame.h"
+
+kGame::kGame()
+{
+
+}
---
+++ kGame.h
@@ -0,0 +1,12 @@
+#ifndef KGAME_H
+#define KGAME_H
+
+#include <kgmGame/kgmGameBase.h>
+
+class kGame : public kgmGameBase
+{
+public:
+  kGame();
+};
+
+#endif // KGAME_H
